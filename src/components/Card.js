f74a4b2b2d0408e1314ec312048bde3ba66ac235
wsuwt <wasuwat.limsuparhat2@lseg.com>
2023-05-04 16:38:11 +0700

    refactor: simplify card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,5 @@
 import styles from './Card.module.css';
 
 export default function Card({ children }) {
-  return (
-    <article className={styles.card}>
-      <h5 className={styles.info}>Select an answer</h5>
-      <section>{children}</section>
-    </article>
-  );
+  return <article className={styles.card}>{children}</article>;
 }
