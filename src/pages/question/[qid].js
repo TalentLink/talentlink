a2b6c98ba59069279b733712bf019030665d36ea
Sarin-Udompanish <sarin.udompanish2@lseg.com>
2023-05-04 16:11:11 +0700

    feat: add zoom animation

--- src/pages/question/[qid].js
+++ src/pages/question/[qid].js
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router';
 import Card from '@/components/Card';
 import Answer from '@/components/Answer';
+import { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 
 import styles from '@/styles/Question.module.css';
 import ProgressBar from '@/components/ProgressBar';
@@ -58,26 +60,50 @@ const QuestionPage = () => {
   const router = useRouter();
   const { qid } = router.query;
   const question = mockupData[qid];
+  const [isZoomedIn, setIsZoomedIn] = useState(false);
+
+  const zoomAnimation = useSpring({
+    transform: isZoomedIn ? 'scale(0.9)' : 'scale(1)',
+    opacity: isZoomedIn ? 0 : 1,
+    config: {
+      duration: 100,
+    },
+    onRest: () => {
+      if (isZoomedIn) {
+        setIsZoomedIn(false);
+      }
+    },
+  });
 
   if (!question) {
     return <div>Not found !</div>;
   }
+
+  const handleClickAnswer = () => {
+    setIsZoomedIn(!isZoomedIn);
+  };
 
   return (
     <div className={styles.container}>
       <div>
         <p className={styles.pageInfo}>{question.depth} of 8</p>
         <ProgressBar count={question.depth} />
-        <Card className={styles.question}>
-          <h2>{question.title}</h2>
-          <br />
-          <br />
-          {question.answers.map((answer) => (
-            <Answer key={answer.to} to={answer.to}>
-              {answer.title}
-            </Answer>
-          ))}
-        </Card>
+        <animated.div style={zoomAnimation}>
+          <Card className={styles.question}>
+            <h2>{question.title}</h2>
+            <br />
+            <br />
+            {question.answers.map((answer) => (
+              <Answer
+                key={answer.to}
+                to={answer.to}
+                handleClick={handleClickAnswer}
+              >
+                {answer.title}
+              </Answer>
+            ))}
+          </Card>
+        </animated.div>
       </div>
     </div>
   );
