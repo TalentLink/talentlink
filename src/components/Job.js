1a90250a7e5740872e1731fb36a4245cce020fef
Wasuwat Limsuparhat <86233706+wsuwt@users.noreply.github.com>
2023-06-15 14:14:31 +0700

    test meta tags

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -20,41 +21,58 @@ export default function Job(props) {
   }
 
   return (
-    <Card>
-      <Image
-        priority
-        src="/engineer.svg"
-        alt="Picture of an engineer"
-        className={styles.image}
-        width={180}
-        height={180}
-      />
-      <h4 className={styles.headline}>{job.headline}</h4>
-      <p className={styles.jobDescription}>{job.description}</p>
-      <p className={styles.jobIs}>{label.jobIs}</p>
-      <h4 className={styles.job}>{job.job}</h4>
+    <>
+      <Head>
+        <title>TalentLink | BKK</title>
+        <meta
+          name="description"
+          content="ลองทำแบบสอบถามเพื่อหาอาชีพสาย Tech ที่เหมาะกับคุณ"
+          key="desc"
+        />
+        <meta property="og:title" content="TalentLink | BKK" />
+        <meta
+          property="og:description"
+          content="ลองทำแบบสอบถามเพื่อหาอาชีพสาย Tech ที่เหมาะกับคุณ"
+        />
+        <meta property="og:image" content="https://picsum.photos/400/500" />
+      </Head>
+      <Card>
+        <Image
+          priority
+          src="/engineer.svg"
+          alt="Picture of an engineer"
+          className={styles.image}
+          width={180}
+          height={180}
+        />
+        <h4 className={styles.headline}>{job.headline}</h4>
+        <p className={styles.jobDescription}>{job.description}</p>
+        <p className={styles.jobIs}>{label.jobIs}</p>
+        <h4 className={styles.job}>{job.job}</h4>
 
-      <section>
-        <Link
-          href={
-            props.mode === 'share'
-              ? 'https://www.facebook.com/sharer.php?u=talentlink.vercel.app&hashtag=%23BKKtalentlink'
-              : '/'
-          }
-          tabIndex={-1}
-        >
-          <Button secondary>
-            {props.mode === 'share' ? label.share : label.tryQuestionaire}
-          </Button>
-        </Link>
-        <Link
-          href="https://refinitiv.wd3.myworkdayjobs.com/en-US/Careers?locationCountry=873d0f604e3b458c990cb4d83a5c0f14"
-          tabIndex={-1}
-          target="_blank"
-        >
-          <Button>Opportunities at LSEG</Button>
-        </Link>
-      </section>
-    </Card>
+        <section>
+          <Link
+            href={
+              props.mode === 'share'
+                ? `https://www.facebook.com/sharer/sharer.php?u=talentlink.vercel.app/job/${jid}/preview/&hashtag=%23BKKtalentlink`
+                : '/'
+            }
+            tabIndex={-1}
+            target="_blank"
+          >
+            <Button secondary>
+              {props.mode === 'share' ? label.share : label.tryQuestionaire}
+            </Button>
+          </Link>
+          <Link
+            href="https://refinitiv.wd3.myworkdayjobs.com/en-US/Careers?locationCountry=873d0f604e3b458c990cb4d83a5c0f14"
+            tabIndex={-1}
+            target="_blank"
+          >
+            <Button>Opportunities at LSEG</Button>
+          </Link>
+        </section>
+      </Card>
+    </>
   );
 }
